39f033d42259ac0f97f029e0ec28596964563e72
axperien <axperien@yandex.ru>
2018-08-17 23:22:35 +0400

    v2

--- src/app.js
+++ src/app.js
@@ -1,56 +1,82 @@
 import React from 'react';
+import moment from 'moment';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+moment.locale("ru");
 
 export default class StopClock extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            currentTime: new Date(),
-            timer: setInterval(() => this.timerTick(this.state.currentTime), 1000),
-            isStopped: false
-        }
+            currentTime: moment(),
+            interval: 1000,
+            isStopped: true
+        };
+        this.changeTime = this.changeTime.bind(this);
     }
 
     timerTick = date => {
-        this.setState({
-            currentTime: new Date(date.setSeconds(date.getSeconds() + 1))
-        });
+        this.timer = setInterval(() => 
+            this.changeTime(date.add(1, 's')), this.state.interval)
     }
 
     stopTime = () => {
-        clearInterval(this.state.timer);
-        this.setState(prevState => ({
-            currentTime: prevState.currentTime,
-            isStopped: !prevState.isStopped
-        }));
+        clearInterval(this.timer);
+        this.setState({isStopped: true});
     }
 
-    startTime = (date = this.state.currentTime) => {
-        clearInterval(this.state.timer);
-        this.setState({
-            isStopped: false,
-            timer: setInterval(() => this.timerTick(date), 1000)
-        });
+    startTime = (date) => {
+        if (this.state.isStopped) {
+            const newDate = date || this.state.currentTime;
+            this.setState({isStopped: false});
+            this.timerTick(newDate);
+        }
     }
+
+    resetTime = () => {
+        if (this.state.isStopped) 
+            this.setState({currentTime: moment()});
+    }
+
+    changeTime = date => this.setState({currentTime: date});
 
     render() {
         return ( 
             <div className="counter">
                 <div className="counter-title">
-                    It's timer. Good luck. :)
+                    It`s timer. Good luck. :)
                 </div>
                 <div className="counter-time"> 
-                    {this.state.currentTime.toLocaleTimeString()}
+                    {this.state.currentTime.format('Do MMMM YYYY, HH:mm:ss')}
                 </div>
                 <div className="counter-status">
                     {this.state.isStopped 
-                        ? (<span>Time current stop</span>) 
-                        : (<span>Time current started</span>) }
+                        ? (<span style={{color: 'red'}}>Time current stop</span>) 
+                        : (<span style={{color: 'green'}}>Time current started</span>) }
                 </div>
                 <div className="counter-buttons">
-                    <button onClick = {() => this.stopTime()} className="counter-button"> Stop </button> 
-                    <button onClick = {() => this.startTime()} className="counter-button"> Start </button>
-                    <button onClick = {() => this.startTime(new Date())} className="counter-button"> Reset </button>
+                    <button onClick = {() => this.stopTime()} className="counter-button">Stop</button> 
+                    <button onClick = {() => this.startTime()} className="counter-button">Start</button>
+                    {this.state.isStopped 
+                        ? (<button onClick = {() => this.resetTime()} className="counter-button">Reset</button>) 
+                        : (<button  className="counter-button counter-button__disabled">Reset</button>)}                     
                 </div> 
+                <div className="datepicker">
+                    <div className="datepicker-title">Choose your time:</div>
+                    <DatePicker
+                        selected={this.state.currentTime}
+                        onChange={this.changeTime}
+                        disabled={!this.state.isStopped}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        dateFormat="LLL"
+                        timeCaption="time"
+                    />
+                </div>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/reset.css';
+import './css/styles.css';
 import StopClock from './app.js';
 
 ReactDOM.render(<StopClock />, document.getElementById('root'));
